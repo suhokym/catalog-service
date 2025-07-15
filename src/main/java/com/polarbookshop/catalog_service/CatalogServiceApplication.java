03a7d75a5afc18ec3c2aab1dfba1b578bab69433
dytpq <suhokym@gmail.com>
2025-07-15 16:57:04 +0900

    wrong spelling change

--- src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatalogServiceApplication {
-	//예
+	//예tl
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
 	}
